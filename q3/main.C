b92e8ff4efcda411e9967a9085ff21802d5a647e
AlberMDVG <117850218+AlberMDVG@users.noreply.github.com>
2023-02-07 15:59:21 +0000

    Create main.C
    New version simpler

---
+++ q3/main.C
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void until_first_negative_number(void)
+{
+    //Define initial variable
+    float a;
+    
+    //Input first value and check if positive. If negative, do not enter the loop.
+    printf("Please input your first value, negative value to exit: \n");
+    scanf("%f", &a);
+    
+    if (a < 0)
+    {
+        /*Exit function*/
+        printf("Exit without any inputs.\n");
+        return;
+    }
+    
+    float min = a, max = a;
+    float rng = 0;
+    int cnt = 0;
+
+    while (a >= 0)
+    {
+        cnt++;
+        //Loop until first negative value is inputted. Only count values up until this point when finding max, min, rng and cnt
+       
+        if (a > max)
+        {
+            max = a;
+        }
+        else if (a < min)
+        {
+            min = a;
+        }
+        rng = max - min;  
+        
+        
+        printf("Please input a new value, negative value to exit: \n");
+        scanf("%f", &a);    
+    }
+
+    printf("\n->Negative number found. \n");
+    printf("Maximum: %f\nMinimum: %f\nRange: %f\nNumber of non-negative values inserted: %d\n", max, min, rng, cnt);
+}
+int main(void)
+{
+    until_first_negative_number();
+    return EXIT_SUCCESS;
+}
