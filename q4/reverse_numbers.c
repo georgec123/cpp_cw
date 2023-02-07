342cf2a01b13d9a153ef898686281b5d0781754a
AlberMDVG <117850218+AlberMDVG@users.noreply.github.com>
2023-02-07 17:26:25 +0000

    Update reverse_numbers.c
    Add some comments

--- q4/reverse_numbers.c
+++ q4/reverse_numbers.c
@@ -1,6 +1,8 @@
 #include "header.h"
+/*This file contains the functions required for the solution of Q4*/
 
-
+/*This function is only used to make sure that the ordinal numbers have the right suffix*/
+/*It returns a chain of characters*/
 char* suffix_for_number(int number) {
 	int last_digit = number % 10;
 	switch (last_digit) {
@@ -15,7 +17,7 @@ char* suffix_for_number(int number) {
 	}
 }
 
-
+/*Function to take an input verifying that it is a number.*/
 float input_digit()
 {
 	float n;
@@ -27,15 +29,17 @@ float input_digit()
 	return n;
 }
 
+/*Recursive function keeps track of the values we have written, using recursion. Parameter is the number of values to take*/
 void reverse_number_print( int n)
 {
 
 	float value;
 	char* suffix = suffix_for_number(numiter - n + 1);
-
+	
 	printf("Input the %d%s value.\n", numiter - n + 1, suffix);
 	value = input_digit();
 
+	/*If after the last value has been inserted we stop recursion*/
 	if (n != 1)
 	{
 		reverse_number_print(n - 1);
