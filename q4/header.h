fef067e3372a7729bad3132fb8d04c10c8105cc5
AlberMDVG <117850218+AlberMDVG@users.noreply.github.com>
2023-02-07 16:36:47 +0000

    Update header.h
    Fixing repeated text

--- q4/header.h
+++ q4/header.h
@@ -1,4 +1,12 @@
 #pragma once
 
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define numiter 200
+
 char* suffix_for_number(int number);
 void reverse_number_print(int n);
