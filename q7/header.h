20b5fcbd5f6cb2cd77087794085051b7cf598a6d
AlberMDVG <117850218+AlberMDVG@users.noreply.github.com>
2023-02-08 15:02:20 +0000

    Create header.h
    header file

---
+++ q7/header.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void series_generator(int n, int* series);
+void series_printer(int n, int* series);
+void eratosthenes(int n, int* series);
+void optimised_eratostenes(int n);
